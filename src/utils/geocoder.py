1b921d3734d736315cea6e5bd8a61972faa5b0c8
faranji <faranjprag@gmail.com>
2026-07-14 13:17:18 +0300

    geocoder nominatim updated

--- src/utils/geocoder.py
+++ src/utils/geocoder.py
@@ -1,5 +1,4 @@
 from geopy.geocoders import Nominatim
-from geopy.exc import GeocoderTimedOut
 
 def get_coordinates(location_name: str) -> tuple:
     """
@@ -10,9 +9,15 @@ def get_coordinates(location_name: str) -> tuple:
     Returns:
         tuple: (latitude, longitude)
     """
-    # TODO 1: Nominatim sınıfını çağır. (user_agent olarak 'gasgraph_app' gibi bir isim ver)
+    # TODO 1: Nominatim sınıfını çağır.
+    locator = Nominatim(user_agent = "gasgraph_app")
+
     # TODO 2: geocode() fonksiyonu ile location_name'i arat.
+    location = locator.geocode(location_name)
+
     # TODO 3: Eğer sonuç (location) bulunursa (location.latitude, location.longitude) tuple'ı döndür.
-    # TODO 4: Bulunamazsa veya hata verirse (None, None) döndür.
-    
+    if location:
+        return  (location.latitude, location.longitude)
+    else:
+        return (None, None)
     pass
